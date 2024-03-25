91af4709b139f09cbc8be1672d156f5ad126cdc7
Bruno Henrique <brunohmfcorporativo@gmail.com>
2024-03-25 20:53:10 -0300

    feat: Adiçaõ atividade

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRollClicked(object sender, EventArgs e)
+        {
+            if ( dropbox.SelectedIndex == -1)
+            {
+                labelDiceValue.Text = "SELECIONE UMA QUANTIDADE DE LADOS.";
+                return;
+            }
+            string dropboxText = dropbox.SelectedItem.ToString();
+
+            int dropboxValue = int.Parse(dropboxText);
+            Random rnd = new Random();
+            int diceResult = rnd.Next(dropboxValue);
+
+
+           labelDiceValue.Text = diceResult.ToString();
+        }
+
+    }
+
+}
